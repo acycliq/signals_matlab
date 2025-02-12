5336034fd8075dfacd881220113fd238c9d679c0
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-02-12 21:33:56 +0000

    rename the overloaded function size because it is getting called by the UI and causes me problems

--- +sig/+node/Signal.m
+++ +sig/+node/Signal.m
@@ -539,7 +539,7 @@ classdef Signal < sig.Signal & handle
       h = onValue(this, @disp);
     end
     
-    function varargout = size(varargin)
+    function varargout = size_ZZZ(varargin)
       % [sz1,...,szN] = size(x) or szdim = size(x,dim) returns the size of
       % signal x, optionally over dimension dim.
       if nargout > 1
