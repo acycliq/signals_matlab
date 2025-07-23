131ed469498301808e2e2d72473bfedaac66accb
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-07-23 13:27:23 +0100

    Add some comments for more clarity

--- +sig/NotSet.m
+++ +sig/NotSet.m
@@ -1,4 +1,17 @@
 classdef (Sealed) NotSet
-    % Just a empty class
+    % Class for uninitialized node values
+    % This class is only used for node initialization. 
+    % Performance-critical code uses boolean flags
+    % (hasCurrValue) instead of expensive isa() type checking.
+    %
+    % INITIALIZATION EXAMPLE:
+    %   currNodeValue = sig.NotSet()  % Clear semantic meaning of "uninitialized"
+    %
+    % PERFORMANCE NOTE: We replaced expensive calls like:
+    %   isa(node.currNodeValue, 'sig.NotSet') 
+    % with fast boolean checks like:
+    %   node.hasCurrValue
+    % I think it will safe to make the class redudant and initialise as a
+    % boolean: currNodeValue = False
 end
 
